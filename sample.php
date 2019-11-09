48b85ee2c4bc3417e317627f34831f31403ae0ac
yoshinobu.takaki <yoshinobutakaki0921@gmail.com>
2019-11-09 11:59:39 +0000

    add sample.phpy

---
+++ sample.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>おみくじ</title>
+    </head>
+    <body>
+        <h1>おみくじ</h1>
+        <p>おみくじ結果：無し</p>
+        <form action="sumple.php" method="POST">
+            <botton type="submit" name="act" value="drow">おみくじを引く！</botton>
+        </form>
+    </body>
+</html>
